f97c72fa7e41d82970dcbe91a9b86a7ae85e2979
krisztiantalan <45624485+krisztiantalan@users.noreply.github.com>
2023-05-18 16:09:00 +0200

    Add files via upload

--- asset_inventory/inventory/models.py
+++ asset_inventory/inventory/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
 
     def __str__(self):
-        return self.nev
+        return self.name
 
     name = models.CharField(max_length=40)
     username = models.CharField(max_length=20, blank=True, unique=True)
@@ -15,9 +15,10 @@ class User(models.Model):
 class Asset(models.Model):
 
     def __str__(self):
-        return self.megnevezes
+        return self.designation
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # when a user is deleted with assets on their name, the assets get moved to stock
+    user = models.ForeignKey(User, default=1, on_delete=models.SET_DEFAULT)
     barcode = models.CharField(max_length=10, unique=True)
     serial_no = models.CharField(max_length=20, blank=True, null=True, unique=True)
     designation = models.CharField(max_length=20)
